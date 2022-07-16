5872b7f8ddaf0c73f865b7efaf085204d1993ad5
pamelsilva <pamelap_silva21@hotmail.com>
2022-07-16 12:46:50 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-import NotificationButton from "./componentes/NotificationButton"
 import Header from "./componentes/Header"
+import SalesCard from "./componentes/SalesCard"
+
 
 function App() {
   return (
     <>
-     <Header />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
